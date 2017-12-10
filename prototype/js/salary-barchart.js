a2fb637362a48ae8697565869418ca2a0eed9baf
xinwen <xliu2@gsd.harvard.edu>
2017-12-10 01:58:54 -0500

    update

--- prototype/js/salary-barchart.js
+++ prototype/js/salary-barchart.js
@@ -20,10 +20,50 @@ document.getElementById("guess2").onclick = function(){
     }
 
     document.getElementById("formGroupExampleInput").style.color = "#FF804F";
+};
+
+
+
+//reveal number
+function revealMaleNum(){
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ',',
+        decimal: '.'
+    };
+    var demo = new CountUp("male-num", 0, 20493, 0, 2.5, options);
+    if (!demo.error) {
+        demo.start();
+    }
+}
+function revealFemaleNum(){
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ',',
+        decimal: '.'
+    };
+    var demo = new CountUp("female-num", 0, 15775, 0, 2.5, options);
+    if (!demo.error) {
+        demo.start();
+    }
 }
 
+/*
+https://stackoverflow.com/questions/3464876/javascript-get-window-x-y-position-for-scroll
+ */
+var firstTime=true;
+function testScroll(ev){
+    console.log("humm"+document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop>600 && firstTime){
 
-
+        revealMaleNum();
+        revealFemaleNum();
+        firstTime=false;
+    };
+}
+window.onscroll=testScroll;
 
 
 
