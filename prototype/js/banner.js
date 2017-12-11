e0176cb82428c22dcc4caf8f6451085b74a365b7
xinwen <xliu2@gsd.harvard.edu>
2017-12-11 18:48:54 -0500

    update

--- prototype/js/banner.js
+++ prototype/js/banner.js
@@ -14,5 +14,22 @@ var button= document.getElementById("bannerbutton");
 button.addEventListener("click",function()
 {
      slider.value=53;
-    output.innerHTML = parseInt(28310+" ");
+    var startNum = slider.value;
+
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ',',
+        decimal: '.'
+    };
+    var demo = new CountUp("demo", startNum, 28310, 0, 2.5, options);
+    if (!demo.error) {
+        demo.start();
+    } else {
+        demo = new CountUp("demo", 0, 28310, 0, 2.5, options);
+        demo.start();
+    }
+
+    output.style.color = "#FF804F";
+
 });
